af4768dd3aadf1e03fac86f11b3393d64229439a
cgeek <cem.moreau@gmail.com>
2017-04-10 08:03:20 +0200

    [enh] `crawl-lookup` now relies on `/wot/requirements-of-pending/x`

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ module.exports = {
       { value: '--nointeractive', desc: 'Disable interactive sync UI.'},
       { value: '--nocautious',    desc: 'Do not check blocks validity during sync.'},
       { value: '--cautious',      desc: 'Check blocks validity during sync (overrides --nocautious option).'},
-      { value: '--nopeers',       desc: 'Do not retrieve peers during sync.'}
+      { value: '--nopeers',       desc: 'Do not retrieve peers during sync.'},
+      { value: '--minsig <minsig>', desc: 'Minimum pending signatures count for `crawl-lookup`. Default is 5.'}
     ],
 
     cli: [{
@@ -181,20 +182,21 @@ module.exports = {
         }
       })
     }, {
-      name: 'crawl-lookup',
+      name: 'crawl-lookup <toHost> <toPort> [<fromHost> [<fromPort>]]',
       desc: 'Make a full network scan and rebroadcast every WoT pending document (identity, certification, membership)',
       onDatabaseExecute: (server, conf, program, params) => co(function*() {
+        const toHost = params[0]
+        const toPort = params[1]
+        const fromHost = params[2]
+        const fromPort = params[3]
         const logger = server.logger;
         try {
           const Peer = server.lib.Peer;
-          const peers = []//yield server.dal.peerDAL.query('SELECT * FROM peer WHERE status = ?', ['UP']);
-          peers.unshift({ endpoints: ['BASIC_MERKLED_API g1.duniter.org 10901'] });
-          // peers.unshift({ endpoints: ['BASIC_MERKLED_API wotex.cgeek.fr 63700'] });
-          // const peers = [{ endpoints: ['BASIC_MERKLED_API cgeek.fr 10900'] }];
+          const peers = fromHost && fromPort ? [{ endpoints: [['BASIC_MERKLED_API', fromHost, fromPort].join(' ')] }] : yield server.dal.peerDAL.query('SELECT * FROM peer WHERE status = ?', ['UP'])
           const mss = {};
           const identities = {};
           const certs = {};
-          const targetPeer = contacter('g1.cgeek.fr', 80, { timeout: 5000 });
+          const targetPeer = contacter(toHost, parseInt(toPort), { timeout: 10000 });
           // Memberships
           for (const p of peers) {
             const peer = Peer.statics.fromJSON(p);
@@ -202,116 +204,76 @@ module.exports = {
             const fromPort = peer.getPort();
             logger.info('Looking at %s:%s...', fromHost, fromPort);
             try {
-              const node = contacter(fromHost, fromPort, { timeout: 3000 });
-              const pendings = (yield node.wotPending()).memberships;
-              const members = (yield node.wotMembers()).results;
-              for (const ms of pendings.concat(members)) {
-                const id = [ms.pubkey, ms.uid].join('-');
-                if (!mss[id]) {
-                  mss[id] = ms;
-                }
-              }
-            } catch (e) {
-              logger.error(e);
-            }
-          }
-          // Identities
-          for (const p of peers) {
-            const peer = Peer.statics.fromJSON(p);
-            const fromHost = peer.getHostPreferDNS();
-            const fromPort = peer.getPort();
-            logger.info('Looking at %s:%s...', fromHost, fromPort);
-            try {
-              const node = contacter(fromHost, fromPort, { timeout: 6000 });
-              const pending = Object.values(mss);
-              for (const ms of pending) {
+              const node = contacter(fromHost, fromPort, { timeout: 10000 });
+              const requirements = yield node.getRequirementsPending(program.minsig || 5);
+              // Identities
+              for (const idty of requirements.identities) {
                 try {
-                  console.log('Lookup %s', ms.uid);
-                  const lookup = yield node.getLookup(ms.uid);
-                  for (const res of lookup.results) {
-                    for (const uid of res.uids) {
-                      const iid = [res.pubkey, uid.uid, uid.meta.timestamp].join('-');
-                      if (!identities[iid]) {
-                        console.log('New identity %s', uid.uid);
-                        identities[iid] = uid;
-                        try {
-                          const rawIdty = common.rawer.getOfficialIdentity({
-                            currency: 'g1',
-                            issuer: res.pubkey,
-                            uid: uid.uid,
-                            buid: uid.meta.timestamp,
-                            sig: uid.self
-                          });
-                          logger.info('Success idty %s', uid.uid);
-                          yield targetPeer.postIdentity(rawIdty);
-                        } catch (e) {
-                          logger.warn(e);
-                        }
-                        // // + Membership
-                        // const id = [ms.pubkey, ms.uid].join('-');
-                        // const theMS = mss[id];
-                        // if (theMS) {
-                        //   console.log('New membership pending for %s', theMS.uid);
-                        //   try {
-                        //     const rawIdty = common.rawer.getMembership({
-                        //       currency: 'g1',
-                        //       issuer: theMS.pubkey,
-                        //       block: [theMS.blockNumber, theMS.blockHash].join('-'),
-                        //       membership: theMS.membership,
-                        //       certts: uid.meta.timestamp,
-                        //       signature: theMS.self
-                        //     });
-                        //     logger.info('Success ms idty %s', uid.uid);
-                        //     yield targetPeer.postIdentity(rawIdty);
-                        //   } catch (e) {
-                        //     logger.warn(e);
-                        //   }
-                        // }
-                      }
-                      for (const received of uid.others) {
-                        const cid = [received.pubkey, iid].join('-');
-                        if (!certs[cid]) {
-                          console.log('New cert %s -> %s', received.pubkey, uid.uid);
-                          yield new Promise((res) => setTimeout(res, 300));
-                          certs[cid] = received;
-                          const rawCert = common.rawer.getOfficialCertification({
-                            currency: 'g1',
-                            issuer: received.pubkey,
-                            idty_issuer: res.pubkey,
-                            idty_uid: uid.uid,
-                            idty_buid: uid.meta.timestamp,
-                            idty_sig: uid.self,
-                            buid: common.buid.format.buid(received.meta.block_number, received.meta.block_hash),
-                            sig: received.signature
-                          });
-                          try {
-                            yield targetPeer.postCert(rawCert);
-                            logger.info('Success cert %s -> %s', received.pubkey, uid.uid);
-                          } catch (e) {
-                            logger.warn(e);
-                          }
-                        }
+                  const iid = [idty.pubkey, idty.uid, idty.meta.timestamp].join('-');
+                  if (!identities[iid]) {
+                    logger.info('New identity %s', idty.uid);
+                    identities[iid] = idty;
+                    try {
+                      const rawIdty = common.rawer.getOfficialIdentity({
+                        currency: 'g1',
+                        issuer: idty.pubkey,
+                        uid: idty.uid,
+                        buid: idty.meta.timestamp,
+                        sig: idty.sig
+                      });
+                      logger.info('Posting idty %s...', idty.uid);
+                      yield targetPeer.postIdentity(rawIdty);
+                      logger.info('Success idty %s', idty.uid);
+                    } catch (e) {
+                      logger.warn(e);
+                    }
+                  }
+                  for (const received of idty.pendingCerts) {
+                    const cid = [received.from, iid].join('-');
+                    if (!certs[cid]) {
+                      logger.info('New cert %s -> %s', received.from, idty.uid);
+                      yield new Promise((res) => setTimeout(res, 300));
+                      certs[cid] = received;
+                      const rawCert = common.rawer.getOfficialCertification({
+                        currency: 'g1',
+                        issuer: received.from,
+                        idty_issuer: idty.pubkey,
+                        idty_uid: idty.uid,
+                        idty_buid: idty.meta.timestamp,
+                        idty_sig: idty.sig,
+                        buid: received.blockstamp,
+                        sig: received.sig
+                      });
+                      try {
+                        yield targetPeer.postCert(rawCert);
+                        logger.info('Success cert %s -> %s', received.from, idty.uid);
+                      } catch (e) {
+                        logger.warn(e);
                       }
                     }
-                    // for (const signed of res.signed) {
-                    //   const iid = [signed.pubkey, signed.uid, signed.meta.timestamp].join('-');
-                    //   const cid = [res.pubkey, iid].join('-');
-                    //   if (!certs[cid]) {
-                    //     console.log('New cert2 %s -> %s', res.pubkey, signed.uid);
-                    //     certs[cid] = signed;
-                    //     // const rawCert = common.rawer.getOfficialCertification({
-                    //     //   currency: 'g1',
-                    //     //   issuer: res.pubkey,
-                    //     //   idty_issuer: res.pubkey,
-                    //     //   idty_uid: signed.uid,
-                    //     //   idty_buid: signed.meta.timestamp,
-                    //     //   idty_sig: signed.self,
-                    //     //   buid: res.meta.timestamp,
-                    //     //   sig: res.signature
-                    //     // });
-                    //     // yield targetPeer.postCert(rawCert);
-                    //   }
-                    // }
+                  }
+                  for (const theMS of idty.pendingMemberships) {
+                    // + Membership
+                    const id = [idty.pubkey, idty.uid, theMS.blockstamp].join('-');
+                    if (!mss[id]) {
+                      mss[id] = theMS
+                      logger.info('New membership pending for %s', idty.uid);
+                      try {
+                        const rawMS = common.rawer.getMembership({
+                          currency: 'g1',
+                          issuer: idty.pubkey,
+                          userid: idty.uid,
+                          block: theMS.blockstamp,
+                          membership: theMS.type,
+                          certts: idty.meta.timestamp,
+                          signature: theMS.sig
+                        });
+                        yield targetPeer.postRenew(rawMS);
+                        logger.info('Success ms idty %s', idty.uid);
+                      } catch (e) {
+                        logger.warn(e);
+                      }
+                    }
                   }
                 } catch (e) {
                   logger.warn(e);
